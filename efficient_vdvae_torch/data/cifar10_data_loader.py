Disable weight decay in Pytroch and fix normalization in cifar-10 dataloader

efficient_vdvae_torch/data/cifar10_data_loader.py
@@ -51,8 +51,9 @@ class MinMax(object):
         :return: Tensor
         """
         img = np.asarray(img)
-        img = img / (2 ** hparams.data.num_bits - 1)
 
+        shift = scale = (2 ** 8 - 1) / 2
+        img = (img - shift) / scale  # Images are between [-1, 1]
         return torch.tensor(img).permute(2, 0, 1).contiguous().float()
 
     def __repr__(self):
@@ -72,6 +73,7 @@ else:
     ])
 
 valid_transform = transforms.Compose([
+    Normalize(),
     MinMax(),
 ])
 

efficient_vdvae_torch/data/mnist_data_loader.py
@@ -13,8 +13,8 @@ def download_mnist_datasets():
     # Ignore labels
     (x_train, _), (x_test, _) = tf.keras.datasets.mnist.load_data()
 
-    x_train = x_train[:, np.newaxis, :, :]  # (60000, 28, 28, 1)
-    x_test = x_test[:, np.newaxis, :, :]  # (10000, 28, 28, 1)
+    x_train = x_train[:, np.newaxis, :, :]  # (60000, 1, 28, 28)
+    x_test = x_test[:, np.newaxis, :, :]  # (10000, 1,  28, 28)
     x_train, x_test = x_train / 255., x_test / 255.
 
     # make images of size 32x32
@@ -42,7 +42,7 @@ class Binarize(object):
 
         :return: Tensor
         """
-        img = torch.Tensor(img)
+        img = torch.tensor(img)
         return torch.Tensor(img.size()).bernoulli_(img)
 
     def __repr__(self):
@@ -80,10 +80,12 @@ class mnist_dataset(torch.utils.data.Dataset):
             raise ValueError(f'Unknown Mode {self.mode}')
 
     def __len__(self):
-        if self.mode == 'div_stats':
-            return round(len(self.images) * hparams.synthesis.div_stats_subset_ratio)
-        else:
+        if self.mode in ['train', 'test', 'encode']:
             return len(self.images)
+        elif self.mode == 'val':
+            return hparams.val.n_samples_for_validation
+        elif self.mode == 'div_stats':
+            return round(len(self.images) * hparams.synthesis.div_stats_subset_ratio)
 
 
 def train_val_data_mnist(world_size, rank):

efficient_vdvae_torch/model/losses.py
@@ -25,20 +25,13 @@ class BernoulliLoss(nn.Module):
         targets = targets[:, :, 2:30, 2:30]
         logits = logits[:, :, 2:30, 2:30]
 
-        # recon = torch.squeeze(torch.maximum(logits, torch.as_tensor(0.)) - logits * targets + torch.log(torch.as_tensor(1.) + torch.exp(-torch.abs(logits))), dim=1)
         loss_value = Bernoulli(logits=logits)
         recon = loss_value.log_prob(targets)
         mean_axis = list(range(1, len(recon.size())))
         per_example_loss = - torch.sum(recon, dim=mean_axis)
-        avg_per_example_loss = per_example_loss / (
-            np.prod([recon.size()[i] for i in mean_axis]))  # B
         scalar = global_batch_size * effective_pixels()
         loss = torch.sum(per_example_loss) / scalar
-        avg_loss = torch.sum(avg_per_example_loss) / (global_batch_size * np.log(
-            2))  # divide by ln(2) to convert to bit range (for visualization purposes only)
-        if hparams.data.dataset_source == 'binarized_mnist':
-            # Convert from bpd to nats for comparison
-            avg_loss = avg_loss * np.log(2.) * effective_pixels()
+        avg_loss = torch.sum(per_example_loss) / global_batch_size
         model_means, log_scales = None, None
         return loss, avg_loss, model_means, log_scales
 

efficient_vdvae_torch/synthesize.py
@@ -64,7 +64,7 @@ def write_image_to_disk(filepath, image):
     assert len(image.shape) == 3
     if hparams.data.dataset_source == ' binarized_mnist':
         assert image.shape[0] == 1
-        image += 255.
+        image *= 255.
     else:
         assert image.shape[0] == 3
         image = np.round(image * 127.5 + 127.5)