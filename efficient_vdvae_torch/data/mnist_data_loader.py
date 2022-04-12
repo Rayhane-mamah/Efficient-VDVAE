Fix KL div scaling issue for MNIST dataset and update MNIST dataloader to not include last batch

efficient_vdvae_torch/data/mnist_data_loader.py
@@ -69,7 +69,7 @@ class mnist_dataset(torch.utils.data.Dataset):
             img = self.images[idx]
             img = transform(img)
             return img
-        elif self.mode in ['val', 'div_stats']:
+        elif self.mode in ['val', 'div_stats', 'test']:
             img = self.images[idx]
             return img
         elif self.mode == 'encode':
@@ -115,13 +115,14 @@ def train_val_data_mnist(world_size, rank):
 
 def synth_mnist_data():
     _, _, test_images = download_mnist_datasets()
-    synth_mnist = mnist_dataset(test_images, 'val')
+    synth_mnist = mnist_dataset(test_images, 'test')
     synth_loader = torch.utils.data.DataLoader(
         dataset=synth_mnist,
         batch_size=hparams.synthesis.batch_size,
         shuffle=True,
         pin_memory=True,
-        num_workers=hparams.run.num_cpus)
+        num_workers=hparams.run.num_cpus,
+        drop_last=True)
     return synth_loader
 
 

efficient_vdvae_torch/synthesize.py
@@ -62,14 +62,19 @@ torch.autograd.profiler.emit_nvtx(False)
 
 def write_image_to_disk(filepath, image):
     assert len(image.shape) == 3
-    if hparams.data.dataset_source == ' binarized_mnist':
+
+    if hparams.data.dataset_source == 'binarized_mnist':
         assert image.shape[0] == 1
         image *= 255.
+        image = image[0]  # Greyscale images need to be in shape [h,w] for Pillow and not [1,h,w]
+        image = image.astype(np.uint8)
+
     else:
         assert image.shape[0] == 3
         image = np.round(image * 127.5 + 127.5)
         image = image.astype(np.uint8)
         image = np.transpose(image, (1, 2, 0))
+
     im = Image.fromarray(image)
     im.save(filepath, format='png')
 
@@ -90,8 +95,8 @@ def reconstruction_mode(artifacts_folder, latents_folder, test_dataset, model, s
     for step, inputs in enumerate(test_dataset):
         inputs = inputs.to(device)
         outputs, reconstruction_loss, kl_div = reconstruction_step(model, inputs, variates_masks=variate_masks)
-
         targets = inputs
+
         nelbo = reconstruction_loss + kl_div
         ssim_per_batch = ssim_metric(targets, outputs, global_batch_size=hparams.synthesis.batch_size)
         ssims += ssim_per_batch