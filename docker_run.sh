f779c40f24bbd69129bf477593baf4792f05147e
rayhane-mamah <rayhane.mamah@gmail.com>
2022-08-15 13:59:36 -0400

    resetting testing variables

--- docker_run.sh
+++ docker_run.sh
@@ -2,4 +2,4 @@
 
 HOST_DATA=`readlink -e ./`;
 CONTAINER_DATA=/workspace/Efficient-VDVAE;
-nvidia-docker run -it --shm-size=128g -v $HOST_DATA:$CONTAINER_DATA disentangled_generative_model_image
+nvidia-docker run -it --shm-size=128g -v $HOST_DATA:$CONTAINER_DATA efficient_vdvae_image
